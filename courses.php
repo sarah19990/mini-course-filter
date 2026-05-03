dc3f6175ec5a0e26d5a8dc7ccc622dfe955c1175
sarah19990 <sara1999200407@gmail.com>
2026-05-03 12:20:12 +0100

    Add courses data structure

---
+++ courses.php
@@ -0,0 +1,28 @@
+<?php
+$courses = [
+    [
+        'id' => 1,
+        'name' => 'Health & Safety',
+        'price' => 99,
+        'category' => 'safety',
+    ],
+    [
+        'id' => 2,
+        'name' => 'Food Safety',
+        'price' => 59,
+        'category' => 'safety',
+    ],
+    [
+        'id' => 3,
+        'name' => 'Safeguarding',
+        'price' => 20,
+        'category' => 'training',
+    ],
+    [
+        'id' => 4,
+        'name' => 'ACT',
+        'price' => 50,
+        'category' => 'training',
+    ],
+];
+?>
